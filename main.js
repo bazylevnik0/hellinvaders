c972cfcdc207fb3c5bbf3f2e6e9a86c0fc4223cb
bazylevnik0 <bazylevnik0@gmail.com>
2024-10-15 17:33:15 +0300

    upload test template

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { VRButton } from 'three/addons/webxr/VRButton.js';
 
 console.log(1)
 
@@ -8,7 +9,9 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
+renderer.xr.enabled = true;
 document.body.appendChild( renderer.domElement );
+document.body.appendChild( VRButton.createButton( renderer ) );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
