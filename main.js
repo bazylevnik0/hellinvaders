fe097b9a950e921724be133531f7255cad87f8c1
bazylevnik0 <bazylevnik0@gmail.com>
2024-10-16 17:22:54 +0300

    add prototype of gun functionality

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import * as THREE from 'three';
 import { VRButton } from 'three/addons/webxr/VRButton.js';
 
-var LEVEL = 100;
-console.log(1)
+var LEVEL = 20;
+console.log(2)
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -19,6 +19,8 @@ camera_group.position.set(0,0,5);
 camera_group.add( camera );
 scene.add(camera_group);
 
+const raycaster = new THREE.Raycaster();
+
 var invaders = [];
 function create_object () {
   let invader_geometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -27,6 +29,7 @@ function create_object () {
       invader.position.y = 50;
       invader.position.x = Math.random()*100-50;
       invader.position.z = Math.random()*100-50;
+      	invader.name = "invader";
       scene.add( invader );
       invaders.push( invader );
 }
@@ -49,6 +52,18 @@ function animate() {
   let   r = Math.floor(Math.random()*(100/LEVEL));
   if ( !r ) create_object();
   for ( let i = 0; i < invaders.length; i++ ) {
+    let camera_xr = renderer.xr.getCamera();
+		let direction = new THREE.Vector3();
+		camera_xr.getWorldDirection(direction)
+		raycaster.set( camera_group.position, direction );
+
+		const intersects = raycaster.intersectObjects( scene.children );
+		for ( let i = 0; i < intersects.length; i ++ ) {
+		  if (intersects[i].object.name == "invader") {
+        intersects[ i ].object.material.color.set( 0xffffff );
+      }
+	  }
+
 	  invaders[i].position.y -= 0.1;
 	  if ( invaders[i].position.y < -1 ) {
 	    scene.remove( invaders[i] );
